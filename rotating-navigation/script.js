1dbd43387cf3d1b2a29a8a2d410892040d7ec245
snimmagadda1 <saiguy@me.com>
2020-12-02 12:13:13 -0600

    rotating nav

---
+++ rotating-navigation/script.js
@@ -0,0 +1,11 @@
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container");
+
+close.addEventListener("click", () => {
+  container.classList.remove("show-nav");
+});
+
+open.addEventListener("click", () => {
+  container.classList.add("show-nav");
+});
