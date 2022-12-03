fc167e548e8d99ea0380f3ccb95554a03cbf15bb
snimmagadda1 <saiguy@me.com>
2022-12-03 13:59:55 -0500

    redid hidden search

--- hidden-search/script.js
+++ hidden-search/script.js
@@ -1,9 +1,9 @@
 const search = document.querySelector('.search')
 const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
-console.log('running')
+
+
 btn.addEventListener('click', () => {
-  console.log('clicked');
     search.classList.toggle('active')
     input.focus()
 })
